7cb2c52f149dfd943201d8f1645c65d1400f711a
Eugene Cheung <ey.cheung@outlook.com>
2014-07-24 18:32:46 -0400

    WIP launch page

--- LaunchPage/app.js
+++ LaunchPage/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 
 var app = express();
+app.listen(3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,21 +22,21 @@ app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.render('index');
+  res.render('index');
 });
 
 app.post('/', function (req, res) {
-    if (req.body.email)
-        fs.appendFile('emails.txt', req.body.email + '\r\n');
+  if (req.body.email)
+    fs.appendFile('emails.txt', req.body.email + '\r\n');
 
-    res.json(!!req.body.email);
+  res.json(!!req.body.email);
 });
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -43,24 +44,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
-
 
 module.exports = app;
