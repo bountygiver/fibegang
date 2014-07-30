8311f20d1acbb6f5ff296386157f7c589ca6c536
Eugene Cheung <ey.cheung@outlook.com>
2014-07-29 21:06:24 -0400

    Fading mic and stuff.

--- LaunchPage/public/js/script.js
+++ LaunchPage/public/js/script.js
@@ -3,6 +3,7 @@ $('#submit').click(function (event) {
   event.preventDefault();
   $.post('/', { email: $('#email').val() }, function (result) {
     if (result) {
+      $('.phone-mic').css('opacity', '0');
       $('.notification').html("<h2>Thanks for your interest! We'll notify you when we're ready.</h2>");
     } else {
       console.log('An error occurred. Please try again.');
@@ -19,4 +20,10 @@ $('#mobile-submit').click(function (event) {
       console.log('An error occurred. Please try again.');
     }
   });
+});
+
+$('#show-form').click(function() {
+  $('.notification').show();
+  $('#email').focus();
+  $(this).hide();
 });
